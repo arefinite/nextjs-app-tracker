33bb1b9d8ffe56c984000602b77bd84b304e9948
arefindev <thearefindev@gmail.com>
2024-08-11 18:10:32 +0600

    favicon updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,6 +27,8 @@ export default function RootLayout({
       }}
     >
       <html lang='en'>
+       
+
         <body className={inter.className}>
           <Providers>
             <section className='flex flex-col min-h-screen'>
